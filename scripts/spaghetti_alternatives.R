8999a0dd47c30ee9a2210f044bd08b057efde966
Michael Clark <micl@umich.edu>
2020-09-07 13:56:11 -0400

    content updated through extensions

--- scripts/spaghetti_alternatives.R
+++ scripts/spaghetti_alternatives.R
@@ -41,14 +41,17 @@ gpa %>%
   ggplot(aes(x = occasion, y = student, fill = gpa)) +
   geom_tile() +
   scico::scale_fill_scico(palette = 'acton', direction = -1) + 
-  transition_time(occasion)
+  transition_time(occasion) 
 
-gpa %>% 
+
+gpa %>%
   ggplot(aes(x = occasion, y = gpa)) +
   geom_point() +
   transition_states(occasion,
                     transition_length = 2,
-                    state_length = 1)
+                    state_length = 1) +
+  # shadow_trail() +
+  shadow_wake(wake_length = .5)
 
 load('https://github.com/m-clark/mixed-models-with-r-workshop-2019/raw/master/data/gpa.RData')
 gpa %>% 
@@ -65,8 +68,29 @@ gpa %>%
   theme_minimal()
 
 
+
+gpa %>%
+  slice(1:120) %>%   # if you don't want to wait
+  ggplot(aes(x = occasion, y = gpa)) +
+  geom_path(aes(group = student, alpha = occasion),
+            color = '#ff5500',
+            arrow = arrow(type = 'closed',
+                          angle = 45,
+                          length = unit(2, 'points')),
+            show.legend = F) +
+  # geom_smooth(
+  #   aes(alpha = occasion),
+  #   se = F,
+  #   color = 'darkred',
+  #   show.legend = F
+  # ) +
+  transition_time(as.integer(student)) +
+  shadow_mark(colour = 'gray50', size = 0.25) +
+  theme_clean()
+
 # will take about 20 sec
-library(lme4); library(gganimate)
+library(lme4)
+library(gganimate)
 sleepstudy %>% 
   ggplot(aes(x = Days, y = Reaction)) +
   geom_path(aes(group = Subject, alpha = Days),
@@ -78,14 +102,17 @@ sleepstudy %>%
   transition_reveal(Days) + 
   theme_minimal()
 
-sleepstudy %>% 
-  mutate(Subject = as.numeric(Subject)) %>% 
+
+sleepstudy %>%
+  mutate(Subject = as.numeric(Subject)) %>%
   ggplot(aes(x = Days, y = Reaction)) +
-  geom_smooth(aes(alpha = Days),
-              se=F,
-              color = 'darkred',
-              show.legend = F) +
-  transition_time(Subject) + 
-  shadow_mark(colour = 'black', size = 0.25)
+  geom_smooth(
+    aes(alpha = Days),
+    se = F,
+    color = 'darkred',
+    show.legend = F
+  ) +
+  transition_time(Subject) +
+  shadow_mark(colour = 'black', size = 0.25) +
   theme_minimal()
 
